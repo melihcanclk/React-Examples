aa288e66301cdb3a1923167c5a82424169f51155
melihcanclk <melihcan.cilek@365technology.net>
2021-11-30 19:26:32 +0300

    buttons designed
    -buttons taken bottom of picture
    -css add for buttons
    -when buttons disappare, these were become middle; now standing where they were before

--- react-router-dom-example/src/pages/User.js
+++ react-router-dom-example/src/pages/User.js
@@ -2,14 +2,15 @@ import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+import "./User.css";
+
 
 function User() {
-    const { id, max } = useParams();
+    let { id, max } = useParams();
     const [photo, setPhoto] = useState();
     const [user, setUser] = useState();
-
-    console.log(id)
-    console.log(max)
+    id = parseInt(id);
+    max= parseInt(max);
 
     useEffect(() => {
         axios(`https://jsonplaceholder.typicode.com/photos/${id}`)
@@ -24,15 +25,25 @@ function User() {
             
     }, [id])
     return (
-        <div>
-            <h2>{user===undefined ? <div>Loading!!!</div> : user.name}</h2>
-            <Link to={(id >= 2) ? `/user/${parseInt(id)-1}-${parseInt(max)}` : `#`}>Previous User</Link>
-            {
-              photo === undefined ? <div>Loading!!!</div> :  
-                <img alt={`${id}th element`} src={photo.url}></img> 
-            }
-            <Link to={(parseInt(id) < parseInt(max)) ? `/user/${parseInt(id)+1}-${parseInt(max)}` : `#`}>Next User</Link>
-        </div>
+
+        (user === undefined ||photo === undefined) ? (<div>Loading!!!</div>) : (
+            <div>
+                <div>
+                    <h2>{user.name}</h2>
+                    {
+                        <img alt={`${id}th element`} src={photo.url}></img> 
+                    }
+                    <div>
+                        {user.phone}
+                    </div>
+                    <div>
+                        {(id >= 2) ? <Link className={"next-prev-buttons prev round"} to={`/user/${id-1}-${max}`}>Previous User</Link> : <Link to={"#"}style={{paddingLeft: "127.75px"}}></Link>}
+                        {(id < max) ? <Link className={"next-prev-buttons next round"} to={`/user/${id+1}-${max}`}>Next User</Link> : <Link to={"#"}style={{paddingLeft: "101.2px"}}></Link>}    
+                    </div>
+                </div>
+                
+            </div>
+        )
     );
     
 }
